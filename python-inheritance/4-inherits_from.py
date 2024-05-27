6078120151e52e8cccd3ccb3e163a22b9f9c77fc
DavidPortillo26 <anicetod03@gmail.com>
2024-05-27 22:13:16 +0000

    File 4 rewriten

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""Module containing inherits_from method"""
+"""
+===================================
+module with method inherits_from
+===================================
+"""
 
 
 def inherits_from(obj, a_class):
-    """returns True if the object is an instance of a class that
-    inherited (directly or indirectly) from the specified class;
-    otherwise False"""
-    return isinstance(obj, a_class) and type(obj) != a_class
+    """Method that return True if an object is an instance of a class
+    that inherited from"""
+
+    return False if type(obj) is a_class else isinstance(obj, a_class)
