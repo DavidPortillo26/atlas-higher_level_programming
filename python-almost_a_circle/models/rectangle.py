7a22a0fe5675e836105b6da88bfd1c7d4bc9ce87
DavidPortillo26 <anicetod03@gmail.com>
2024-05-28 22:00:23 +0000

    ws

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -89,7 +89,7 @@ class Rectangle(Base):
         if self.width == 0 or self.height == 0:
             print("")
             return
-    
+
         [print("") for _ in range(self.y)]
         for _ in range(self.height):
             [print(" ", end="") for _ in range(self.x)]
