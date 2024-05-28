2e15618d94e1415d38c26fbbf94e1939ea22569e
DavidPortillo26 <anicetod03@gmail.com>
2024-05-28 20:40:14 +0000

    load from file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -40,4 +40,14 @@ class Base:
         with open("{}.json".format(cls.__name__), "w", encoding="utf-8") as f:
             f.write(cls.to_json_string(list_objs))
     
+    @classmethod
+    def load_from_file(cls):
+        '''Loads string from file and unjsonifies.'''
+        from os import path
+        file = "{}.json".format(cls.__name__)
+        if not path.isfile(file):
+            return []
+        with open(file, "r", encoding="utf-8") as f:
+            return [cls.create(**d) for d in cls.from_json_string(f.read())]
+    
     
