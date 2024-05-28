5a67d4ee521c4af6c439c5bf146d7c0f24d56f04
DavidPortillo26 <anicetod03@gmail.com>
2024-05-28 21:01:37 +0000

    square update and rewrite ws

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -14,17 +14,17 @@ class Square(Rectangle):
         '''Returns string info about this square.'''
         return '[{}] ({}) {}/{} - {}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
-    
+
     @property
     def size(self):
         '''Size of this square.'''
         return self.width
-    
+
     @size.setter
     def size(self, value):
         self.width = value
         self.height = value
-    
+
     def __update(self, id=None, size=None, x=None, y=None):
         '''Internal method that updates instance attributes via */**args.'''
         if id is not None:
@@ -35,14 +35,14 @@ class Square(Rectangle):
             self.x = x
         if y is not None:
             self.y = y
-    
+
     def update(self, *args, **kwargs):
         '''Updates instance attributes via no-keyword & keyword args.'''
         if args:
             self.__update(*args)
         elif kwargs:
             self.__update(**kwargs)
-    
+
     def to_dictionary(self):
         '''Returns dictionary representation of this class.'''
         return {"id": self.id, "size": self.width,
